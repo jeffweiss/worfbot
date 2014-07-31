77a3e72e911bd1a52fdad3cef91d7233f41b23a1
Jeff Weiss <jeff.weiss@puppetlabs.com>
2014-07-31 09:55:11 -0700

    don't have all handlers log debug messages

--- lib/worfbot/default_handler.ex
+++ lib/worfbot/default_handler.ex
@@ -41,81 +41,81 @@ defmodule Worfbot.DefaultHandler do
 
       """
       def handle_info({:connected, server, port}, state) do
-        debug "Connected to #{server}:#{port}"
+        #debug "Connected to #{server}:#{port}"
         {:noreply, state}
       end
       def handle_info(:logged_in, state) do
-        debug "Logged in to server"
+        #debug "Logged in to server"
         {:noreply, state}
       end
       def handle_info(:disconnected, state) do
-        debug "Disconnected from server"
+        #debug "Disconnected from server"
         {:noreply, state}
       end
       def handle_info({:joined, channel}, state) do
-        debug "Joined #{channel}"
+        #debug "Joined #{channel}"
         {:noreply, state}
       end
       def handle_info({:joined, channel, user}, state) do
-        debug "#{user} joined #{channel}"
+        #debug "#{user} joined #{channel}"
         {:noreply, state}
       end
       def handle_info({:topic_changed, channel, topic}, state) do
-        debug "#{channel} topic changed to #{topic}"
+        #debug "#{channel} topic changed to #{topic}"
         {:noreply, state}
       end
       def handle_info({:nick_changed, nick}, state) do
-        debug "We changed our nick to #{nick}"
+        #debug "We changed our nick to #{nick}"
         {:noreply, state}
       end
       def handle_info({:nick_changed, old_nick, new_nick}, state) do
-        debug "#{old_nick} changed their nick to #{new_nick}"
+        #debug "#{old_nick} changed their nick to #{new_nick}"
         {:noreply, state}
       end
       def handle_info({:parted, channel}, state) do
-        debug "We left #{channel}"
+        #debug "We left #{channel}"
         {:noreply, state}
       end
       def handle_info({:parted, channel, nick}, state) do
-        debug "#{nick} left #{channel}"
+        #debug "#{nick} left #{channel}"
         {:noreply, state}
       end
       def handle_info({:invited, by, channel}, state) do
-        debug "#{by} invited us to #{channel}"
+        #debug "#{by} invited us to #{channel}"
         {:noreply, state}
       end
       def handle_info({:kicked, by, channel}, state) do
-        debug "We were kicked from #{channel} by #{by}"
+        #debug "We were kicked from #{channel} by #{by}"
         {:noreply, state}
       end
       def handle_info({:kicked, nick, by, channel}, state) do
-        debug "#{nick} was kicked from #{channel} by #{by}"
+        #debug "#{nick} was kicked from #{channel} by #{by}"
         {:noreply, state}
       end
       def handle_info({:received, message, from}, state) do
-        debug "#{from} sent us a private message: #{message}"
+        #debug "#{from} sent us a private message: #{message}"
         {:noreply, state}
       end
       def handle_info({:received, message, from, channel}, state) do
-        debug "#{from} sent a message to #{channel}: #{message}"
+        #debug "#{from} sent a message to #{channel}: #{message}"
         {:noreply, state}
       end
       def handle_info({:mentioned, message, from, channel}, state) do
-        debug "#{from} mentioned us in #{channel}: #{message}"
+        #debug "#{from} mentioned us in #{channel}: #{message}"
         {:noreply, state}
       end
       # This is an example of how you can manually catch commands if ExIrc.Client doesn't send a specific message for it
       def handle_info(%IrcMessage{:nick => from, :cmd => "PRIVMSG", :args => ["testnick", msg]}, state) do
-        debug "Received a private message from #{from}: #{msg}"
+        #debug "Received a private message from #{from}: #{msg}"
         {:noreply, state}
       end
       def handle_info(%IrcMessage{:nick => from, :cmd => "PRIVMSG", :args => [to, msg]}, state) do
-        debug "Received a message in #{to} from #{from}: #{msg}"
+        #debug "Received a message in #{to} from #{from}: #{msg}"
         {:noreply, state}
       end
       # Catch-all for messages you don't care about
       def handle_info(msg, state) do
-        debug "Received IrcMessage:" <> inspect(msg)
+        #debug "Received IrcMessage:" <> inspect(msg)
         {:noreply, state}
       end
 
